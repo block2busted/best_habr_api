2d277572a6e2f41a9d6073cef62761b01cd9a876
Blockbusted <block2busted@gmail.com>
2020-09-18 16:56:18 +0300

    Added namespace to articles-api urls

--- best_habr_api/apps/articles/api/urls.py
+++ best_habr_api/apps/articles/api/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import ArticleListAPIView, ArticleDetailAPIView
 
 
+app_name = 'articles-api'
 urlpatterns = [
     path('', ArticleListAPIView.as_view(), name='articles'),
-    path('<int:pk>/', ArticleDetailAPIView.as_view(), name='article-detail')
+    path('<int:pk>/', ArticleDetailAPIView.as_view(), name='article-detail'),
 ]
--- best_habr_api/urls.py
+++ best_habr_api/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/articles/', include('articles.api.urls'))
+    path('api/articles/', include('articles.api.urls', namespace='articles-api'))
 ]
